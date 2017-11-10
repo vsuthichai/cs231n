fe2dab31eee345eb277bd79ba82a778e02420e72
Victor Suthichai <victor.suthichai@gmail.com>
2017-11-10 13:51:07 -0800

    3 layer cnn

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -48,7 +48,20 @@ class ThreeLayerConvNet(object):
         # hidden affine layer, and keys 'W3' and 'b3' for the weights and biases   #
         # of the output affine layer.                                              #
         ############################################################################
-        pass
+        
+        C, H, W = input_dim
+        pad = (filter_size - 1) / 2
+        pool_stride, pool_height, pool_width = 2, 2, 2
+        pool_layer_height = int((H - pool_height) / pool_stride + 1)
+        pool_layer_width  = int((W - pool_width)  / pool_stride + 1)
+        
+        self.params['W1'] = np.random.randn(num_filters, C, filter_size, filter_size) * weight_scale
+        self.params['b1'] = np.zeros((num_filters,))
+        self.params['W2'] = np.random.randn(num_filters * pool_layer_height * pool_layer_width, hidden_dim) * weight_scale
+        self.params['b2'] = np.zeros((hidden_dim,))
+        self.params['W3'] = np.random.randn(hidden_dim, num_classes) * weight_scale
+        self.params['b3'] = np.zeros((num_classes,))
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -80,7 +93,15 @@ class ThreeLayerConvNet(object):
         # computing the class scores for X and storing them in the scores          #
         # variable.                                                                #
         ############################################################################
-        pass
+        
+        conv_layer1, conv_cache = conv_forward_fast(X, W1, b1, conv_param)
+        relu_layer1, relu_cache = relu_forward(conv_layer1)
+        pool_layer1, pool_cache = max_pool_forward_fast(relu_layer1, pool_param)
+        pool_flat1 = pool_layer1.reshape(X.shape[0], -1)
+        Z2, Z2_cache = affine_forward(pool_flat1, W2, b2)
+        A2, A2_cache = relu_forward(Z2)
+        Z3, Z3_cache = affine_forward(A2, W3, b3)
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -95,7 +116,26 @@ class ThreeLayerConvNet(object):
         # data loss using softmax, and make sure that grads[k] holds the gradients #
         # for self.params[k]. Don't forget to add L2 regularization!               #
         ############################################################################
-        pass
+        
+        loss, dout = softmax_loss(Z3, y)
+        reg_term = 0.5 * self.reg * (np.sum(W1 * W1) + np.sum(W2 * W2) + np.sum(W3 * W3))
+        loss = loss + reg_term
+        
+        dZ3, dW3, db3 = affine_backward(dout, Z3_cache)
+        dA2 = relu_backward(dZ3, A2_cache)
+        dpool_flat1, dW2, db2 = affine_backward(dA2, Z2_cache)
+        dpool_layer1 = dpool_flat1.reshape(pool_layer1.shape)
+        drelu_layer1 = max_pool_backward_fast(dpool_layer1, pool_cache)
+        dconv_layer1 = relu_backward(drelu_layer1, relu_cache)
+        dX, dW1, db1 = conv_backward_fast(dconv_layer1, conv_cache)
+        
+        grads['W1'] = dW1 + (self.reg * W1)
+        grads['b1'] = db1
+        grads['W2'] = dW2 + (self.reg * W2)
+        grads['b2'] = db2
+        grads['W3'] = dW3 + (self.reg * W3)
+        grads['b3'] = db3
+        
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -25,7 +25,9 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
+    
     out = np.dot(x.reshape(x.shape[0], -1), w) + b.reshape(1, -1)
+    
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
